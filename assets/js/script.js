f7bb226308266117167f56ef21f8d2e8964f9d3c
shakil10945 <shakil10945@gmail.com>
2024-12-27 21:23:24 +0600

    sone

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Simple navigation toggle for mobile
+const navToggle = document.getElementById('navToggle');
+const mainNav = document.getElementById('mainNav');
+
+navToggle.addEventListener('click', () => {
+  if (mainNav.style.display === 'flex') {
+    mainNav.style.display = 'none';
+  } else {
+    mainNav.style.display = 'flex';
+  }
+});
