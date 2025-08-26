c3c5406f1cfed6e8ff19d8fba95aa7ec25f29935
Shakil Khan <shakil10945@gmail.com>
2025-08-27 02:12:32 +0600

    now

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,10 @@ const mainNav = document.getElementById('mainNav');
 navToggle.addEventListener('click', () => {
   if (mainNav.style.display === 'flex') {
     mainNav.style.display = 'none';
-  } else {
+  } 
+  else 
+  {
     mainNav.style.display = 'flex';
   }
-});
+}
+);
